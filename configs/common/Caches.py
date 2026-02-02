dae935a1376d6b217d9a32c005144fc194e46319
NathanChiang <chc10@illinois.edu>
2026-02-02 15:24:45 -0600

    cpu: go fuck yourself
    Change-Id: Ia9a79d61ce475ea29db65967a94aede7804803c1

--- configs/common/Caches.py
+++ configs/common/Caches.py
@@ -71,6 +71,15 @@ class L2Cache(Cache):
     tgts_per_mshr = 12
     write_buffers = 8
 
+class L3Cache(Cache):
+    assoc = 16
+    tag_latency = 24
+    data_latency = 24
+    response_latency = 24
+    mshrs = 512
+    tgts_per_mshr = 20
+    write_buffers = 256
+
 class IOCache(Cache):
     assoc = 8
     tag_latency = 50
